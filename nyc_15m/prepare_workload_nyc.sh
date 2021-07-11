cd5eaa189c97e3990333540af8d6910ecbf853d0
malivamlvis <maliva.ml.vis@gmail.com>
2021-07-10 19:32:15 -0700

    chmod +x run_prepare_sample_tables_nyc.sh; reduce generated queries to 400;

--- nyc_15m/prepare_workload_nyc.sh
+++ nyc_15m/prepare_workload_nyc.sh
@@ -1,14 +1,14 @@
 date
+echo "run_prepare_sample_tables_nyc.sh"
+./run_prepare_sample_tables_nyc.sh
+
+date
 echo "run_generate_queries_nyc.sh"
 ./run_generate_queries_nyc.sh
 
 date
 echo "run_label_queries_nyc.sh"
 ./run_label_queries_nyc.sh
-
-date
-echo "run_prepare_sample_tables_nyc.sh"
-./run_prepare_sample_tables_nyc.sh
 
 date
 echo "run_label_sel_queries_nyc.sh"
