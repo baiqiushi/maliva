b515276dc6e728b627c4c205452bda40347818fe
malivamlvis <maliva.ml.vis@gmail.com>
2021-07-11 14:26:53 -0700

    use sels on 1500k sample table for training query time estimator;

--- nyc_15m/prepare_sample_tables_nyc.sql
+++ nyc_15m/prepare_sample_tables_nyc.sql
@@ -17,3 +17,12 @@ CREATE INDEX idx_nyc_600k_trip_distance ON nyc_600k USING BTREE (trip_distance);
 -- create rtree on pickup_coordinates
 CREATE INDEX idx_nyc_600k_pickup_coordinates ON nyc_600k USING RTREE (pickup_coordinates);
 
+-- 10% sample table
+CREATE TABLE nyc_1500k AS SELECT * FROM nyc_15m TABLESAMPLE BERNOULLI(10);
+-- create btree on pickup_datetime
+CREATE INDEX idx_nyc_1500k_pickup_datetime ON nyc_1500k USING BTREE (pickup_datetime);
+-- create btree on trip_distance
+CREATE INDEX idx_nyc_1500k_trip_distance ON nyc_1500k USING BTREE (trip_distance);
+-- create rtree on pickup_coordinates
+CREATE INDEX idx_nyc_1500k_pickup_coordinates ON nyc_1500k USING RTREE (pickup_coordinates);
+
--- nyc_15m/run_collect_sels_nyc.sh
+++ nyc_15m/run_collect_sels_nyc.sh
@@ -4,5 +4,5 @@
 python3 -u ../smart_collect_sel_queries_nyc.py -if ./queries_nyc.csv -t nyc_150k 2>&1 | tee collect_sel_nyc_150k_queries_nyc.log
 python3 -u ../smart_collect_sel_queries_nyc.py -if ./queries_nyc.csv -t nyc_600k 2>&1 | tee collect_sel_nyc_600k_queries_nyc.log
 
-# use 15m sample table sels for query_estimator training
-python3 -u ../smart_collect_sel_queries_nyc.py -if ./queries_nyc.csv -t nyc_15m 2>&1 | tee collect_sel_nyc_15m_queries_nyc.log
+# use 1500k sample table sels for query_estimator training
+python3 -u ../smart_collect_sel_queries_nyc.py -if ./queries_nyc.csv -t nyc_1500k 2>&1 | tee collect_sel_nyc_1500k_queries_nyc.log
